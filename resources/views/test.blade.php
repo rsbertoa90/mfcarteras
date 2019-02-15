83ec33bad05f42e00b442f105aa8d74d0285a41c
rsbertoa90 <rsbertoa90@gmail.com>
2019-02-15 19:59:47 -0300

    360 model on detail

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="row">
     <div class="col-12 col-lg-4">
-        <variant-model :variant_id={{$id}}></variant-model>
+        <variant-model :variant_id={{$id}} alt="360 test"></variant-model>
     </div>
 </div>
 @endsection
