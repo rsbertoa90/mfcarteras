5c12f19f19228a9bd4272ec90d118ea9c4664825
rsbertoa90 <rsbertoa90@gmail.com>
2019-09-10 18:06:59 -0300

    carteras

--- resources/assets/js/components/home/title.vue
+++ resources/assets/js/components/home/title.vue
@@ -2,7 +2,7 @@
        <div class="row">
                 <div class="d-flex flex-column col-12 text-center justify-content-center align-items-center">
                     <h1>MF Carteras</h1>
-                    <h2>importador  mayorista en el corazón de Once, Envíos a todo el pais</h2>
+                    <h2>importador directo en el corazón de Once, Envíos a todo el pais</h2>
                 </div>
         </div>
 </template>
