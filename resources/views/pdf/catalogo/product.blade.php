50734fe60111819ae17960321be2a0229e613e79
rsbertoa90 <rsbertoa90@gmail.com>
2019-09-10 18:16:31 -0300

    carteras

--- resources/views/pdf/catalogo/product.blade.php
+++ resources/views/pdf/catalogo/product.blade.php
@@ -6,7 +6,7 @@
                 <td>
                     @if (isset($product->images[0]))
                
-                        <img style="width:150px; "  src="{{public_path().$product->images[0]}}"> 
+                        <img style="width:150px; "  src="{{public_path().$product->images[0]->url}}"> 
                     @endif
                     @if (!isset($product->images[0]))
                         <img style="width:150px; "  src="{{public_path().'/storage/images/app/no-image.png'}}"> 
